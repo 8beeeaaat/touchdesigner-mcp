3eea31277c88391c14c40014dea5820b7fd10e1e
8beeeaaat <8beeeaaat@gmail.com>
2025-04-19 08:20:26 +0900

    fix CHOPs

--- src/nodeSchemas/chops/null.ts
+++ src/nodeSchemas/chops/null.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { createCHOPSchema } from "./utils.js";
 
-export const nullChop = createCHOPSchema({
+export const nullNode = createCHOPSchema({
 	cooktype: z.string().optional().describe("Cook Type"),
 	checkvalues: z.boolean().optional().describe("Check Values"),
 	checknames: z.boolean().optional().describe("Check Names"),
--- src/nodeSchemas/chops/out.ts
+++ src/nodeSchemas/chops/out.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { createCHOPSchema } from "./utils.js";
 
-export const outChop = createCHOPSchema({
+export const outNode = createCHOPSchema({
 	label: z.string().optional().describe("Label"),
 	connectorder: z.number().optional().describe("Connect Order"),
 });
