7a0337ec3ca45373d672fb339d4a7d74dd2e2a3a
sadao komaki <8beeeaaat@gmail.com>
2025-05-13 00:22:23 +0900

    0.2.10 (#58)
    * 0.2.9
    
    * 0.2.10
    
    * Refactor Docker setup and environment variables for MCP server (#56)
    
    * Revert "Revert "0.2.9 (#54)" (#55)"
    
    This reverts commit 389e7a9bd08a1cf19dbbc78807c5c1605eda32c5.
    
    * update: refactor Docker setup and environment variables for improved MCP server configuration
    
    * update npm packages (#57)
    
    * update README
    
    * update image

--- src/api/customInstance.ts
+++ src/api/customInstance.ts
@@ -1,9 +1,9 @@
 import Axios, { type AxiosError, type AxiosRequestConfig } from "axios";
 
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:9981";
-export const AXIOS_INSTANCE = Axios.create({
-	baseURL: API_BASE_URL,
-});
+const API_BASE_URL = `${process.env.TD_WEB_SERVER_HOST}:${process.env.TD_WEB_SERVER_PORT}`;
+export const AXIOS_INSTANCE = Axios.create(
+	API_BASE_URL ? { baseURL: API_BASE_URL } : {},
+);
 
 export const customInstance = <T>(
 	config: AxiosRequestConfig,
