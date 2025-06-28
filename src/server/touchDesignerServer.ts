ca01e071b0ce00e3d3f331aa7345d1d6384889b7
8beeeaaat <8beeeaaat@gmail.com>
2025-06-28 17:12:46 +0900

    0.4.0-alpha.2

--- src/server/touchDesignerServer.ts
+++ src/server/touchDesignerServer.ts
@@ -25,7 +25,7 @@ export class TouchDesignerServer {
 		this.server = new McpServer(
 			{
 				name: "TouchDesigner",
-				version: "0.4.0-alpha.1",
+				version: "0.4.0-alpha.2",
 			},
 			{
 				capabilities: {
