930d9f5b3a815463f294a826fe91249145978609
8beeeaaat <8beeeaaat@gmail.com>
2025-06-28 20:47:55 +0900

    release: bump version to 0.4.0-alpha.4 and update dependencies for stability and security

--- src/server/touchDesignerServer.ts
+++ src/server/touchDesignerServer.ts
@@ -25,7 +25,7 @@ export class TouchDesignerServer {
 		this.server = new McpServer(
 			{
 				name: "TouchDesigner",
-				version: "0.4.0-alpha.3",
+				version: "0.4.0-alpha.4",
 			},
 			{
 				capabilities: {
